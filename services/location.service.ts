0df80fa220753caec5f49c126ced0171c8e7b947
Esteban Navarro Martínez <esteban.navarro@akros.tech>
2026-01-30 12:53:10 -0600

    update

--- services/location.service.ts
+++ services/location.service.ts
@@ -2,8 +2,17 @@ import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 import { ApiResponse } from '../types/api-response.type';
 import { LocationType } from '../types/location.type';
+import { HandleResponse } from '../utils/api-response.utils';
+import { Keys, StorageService } from './storage.service';
 
 const api = `${Constants.expoConfig?.extra?.apiUrl}/location`;
+
+export interface ILocation {
+  City: string;
+  Country: string;
+  Latitude: number;
+  Longitude: number;
+}
 
 const handleResponse = async <T>(response: Response): Promise<T> => {
 
@@ -50,7 +59,7 @@ export const LocationService = {
 
       if (geocode?.length > 0) {
         const { city, country } = geocode[0];
-        return { city, country };
+        return { City: city, Country: country, Latitude: location.coords.latitude, Longitude: location.coords.longitude };
       }
 
     } catch (error) {
@@ -62,8 +71,75 @@ export const LocationService = {
         country: null,
         error: 'Ubicación no encontrada'
       };
-  }
+  },
 
+  async SaveLocation(location: ILocation): Promise<ApiResponse<string>> {
+
+    const response = await fetch(api, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(location)
+    });
+
+    return await HandleResponse<string>(response);
+  },
+
+  async GetLocations(): Promise<ApiResponse<ILocation[]>> {
+    
+    const response = await fetch(api, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    return await HandleResponse<ILocation[]>(response);
+  },
+
+  async DeleteLocation(locationId: string): Promise<ApiResponse<boolean>> {
+
+    const response = await fetch(`${api}/${locationId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    
+    return await HandleResponse<boolean>(response);
+  },
+
+  async LoadUserLocation(): Promise<ILocation | null> {
+    
+      const hasAskedLocation = await StorageService.Get(Keys.HasAskedLocation);
+      
+      if (!hasAskedLocation) {
+
+        const result = await this.GetCurrentPosition();
+        
+        if (result) {
+
+          const newLocation: ILocation = {
+            City: result.city || '',
+            Country: result.country || '',
+            Latitude: result.Latitude || 0,
+            Longitude: result.Longitude || 0
+          };
+          
+          const response = await this.SaveLocation(newLocation);
+
+          if (response.Successful) {
+            await StorageService.Save(Keys.HasAskedLocation, 'true');
+            return newLocation;
+          } else {
+            await StorageService.Save(Keys.HasAskedLocation, 'false');
+          }
+        }
+
+      }
+      return null;
+    }
 
 };
 
